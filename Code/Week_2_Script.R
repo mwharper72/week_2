01da0b23c14e77b107c549fa889583848c772da1
Michael Harper <mharper@seattleu.edu>
2021-01-23 18:27:05 -0800

    Final try with partial data.  Unable to run full data set on own machine or SeattleU remote desktop

--- Code/Week_2_Script.R
+++ Code/Week_2_Script.R
@@ -13,6 +13,7 @@ View(nfhs)
 
 house_df <- nfhs %>%
   select('hhid':'hv208', 'hv270')
+write.csv(house_df, file = "Raw Data/hh_data_df.csv")
 
 #Wealth index
 #1  Poorest
@@ -26,6 +27,7 @@ house_df <- nfhs %>%
 #and their education in single years.
 df_educ <- nfhs %>%
   select('hhid', starts_with('hvidx'), starts_with('hv108'))
+
 
 ## anthropometric data for females and the household id
 fem_surv <- nfhs %>%
@@ -62,16 +64,18 @@ education <- education %>%
   separate(codes, c("category", "years"), sep = "_")
 
 education1 <- drop_na(education)
-education2 <- spread(education1, key = "category", value = "n")
-education2 <- education2 %>% rename("roster_id" = "years")
+education1 <- spread(education1, key = "category", value = "n")
+education1 <- education1 %>% rename("roster_id" = "years")
+
+write.csv(education1, file = "Raw Data/education_df.csv")
 
 ## Female Survey
 #sample_frac::small_df <- big_df %>% sample_frac(0.1)
 # test sample
 
-#fe_small <- fem_surv %>% sample_frac(0.1)
+female <- fem_surv %>% sample_frac(0.1)
 
-female <- fem_surv %>%
+female <- female %>%
   gather(key = "category", value = "n", 2:419)
 
 female <- female %>%
@@ -84,16 +88,20 @@ female1 <- spread(female1, key = "category", value = "n")
 female1 <-  female1 %>%
   select('hhid':'ha1', 'ha2', 'ha3') %>%
   drop_na()
-vector_t <- rep(TRUE,#<need length>#) ## Create TRUE
+vector_t <- rep('female', 13942) ## Create TRUE
 female1$gender <- vector_t #Add TRUE to DataFrame
+female1 <- rename(female1, 'ID' = 'ha0', 'age' = 'ha1', 'weight' = 'ha2', 'height' = 'ha3')
+write.csv(female1, file = "Raw Data/female_df.csv")
 
 ## Male Survey
+
+male <- male_surv %>% sample_frac(0.1)
   
-male <- male_surv %>%
+male <- male %>%
     gather(
       key = "category",
       value = "n",
-      ##FIX ME how many variables?)
+      2:667)
       
 male <- male %>%
   separate(category, c("category", "roster_id"), sep = "_")
@@ -103,20 +111,26 @@ male1 <- drop_na(male)
 male1 <- spread(male1, key = "category", value = "n")
       
 male1 <-  male1 %>%
-  elect('hhid':'hb1', 'hb2', 'hb3') %>%
+  select('hhid':'hb1', 'hb2', 'hb3') %>%
   drop_na()
-vector_f <-rep(FALSE, #<need length>#)
-male1$gender <- vector_f #add FALSE to DataFrame
+vector <-rep('male', 9039)
+male1$gender <- vector #add FALSE to DataFrame
+male1 <- rename(male1, 'ID' = 'hb0', 'age' = 'hb1', 'weight' = 'hb2', 'height' = 'hb3')
+write.csv(male1, file = "Raw Data/male_df.csv")
           
 #Stack female and male data frame 
 gender_df <- bind_rows(female1, male1)
+write.csv(gender_df, file = "Raw Data/male_female_df.csv")
           
 #merge education data
-final_df <- gender_df %>% left_join(df_educ, by = "hhid")
-final_df <- final_df %>% left_join(house_df, by = "hhid")
+final_df <- gender_df %>% semi_join(education1, by = "hhid")
+final_df <- final_df %>% semi_join(house_df, by = "hhid")
           
 #median age of males | females
-by_gender <- group_by(final_df, gender)
-summarise(by_gender, median = median(gender, na.rm = TRUE))
-          
+median_age <- final_df %>%
+  select(gender, age) %>%
+  group_by(gender)
+median_age$age <- as.double(median_age$age) #coerce <chr> into <dbl>
+summarise(median_age, age = median(age))       #summarise   
+    
           
